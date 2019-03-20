6f7f8c2bc7ad63fa46a29a490266f1a84232f762
AL10120 <allan.leveille@ocsbstudent.ca>
2019-03-20 12:57:00 -0400

    Walking Man done

---
+++ WalkingManAllanL/WalkingManAllanL/WalkingManForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WalkingManAllanL
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWalk = new System.Windows.Forms.Button();
+            this.picMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(111, 41);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(119, 49);
+            this.btnWalk.TabIndex = 0;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.btnWalk_Click);
+            // 
+            // picMan
+            // 
+            this.picMan.BackgroundImage = global::WalkingManAllanL.Properties.Resources.walk1;
+            this.picMan.Image = global::WalkingManAllanL.Properties.Resources.walk1;
+            this.picMan.Location = new System.Drawing.Point(84, 114);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(167, 182);
+            this.picMan.TabIndex = 2;
+            this.picMan.TabStop = false;
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 308);
+            this.Controls.Add(this.picMan);
+            this.Controls.Add(this.btnWalk);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man by Allan L";
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWalk;
+        private System.Windows.Forms.PictureBox picMan;
+    }
+}
+
---
+++ WalkingManAllanL/WalkingManAllanL/WalkingManForm.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Created by: Allan L
+ * Created on: March 19, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #21 - Walking Man
+ * This program...
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Sleep Timer
+using System.Threading;
+
+namespace WalkingManAllanL
+{
+    public partial class frmWalkingMan : Form
+    {
+        public frmWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWalk_Click(object sender, EventArgs e)
+        {
+            {
+                //local variables and constants
+                const byte MAX_FRAMES = 10;
+                int pictureFrameCounter = 1;
+
+                //Continue the loop as long as the counter has not reach the max number of frames
+                while (pictureFrameCounter < MAX_FRAMES + 1)
+                {
+                    if (pictureFrameCounter == 1)
+                    {
+                        this.picMan.Image = Properties.Resources.walk1;
+                    }
+                    else if (pictureFrameCounter == 2)
+                    {
+                        this.picMan.Image = Properties.Resources.walk2;
+                    }
+                    else if (pictureFrameCounter == 3)
+                    {
+                        this.picMan.Image = Properties.Resources.walk3;
+                    }
+                    else if (pictureFrameCounter == 4)
+                    {
+                        this.picMan.Image = Properties.Resources.walk4;
+                    }
+                    else if (pictureFrameCounter == 5)
+                    {
+                        this.picMan.Image = Properties.Resources.walk5;
+                    }
+                    else if (pictureFrameCounter == 6)
+                    {
+                        this.picMan.Image = Properties.Resources.walk6;
+                    }
+                    else if (pictureFrameCounter == 7)
+                    {
+                        this.picMan.Image = Properties.Resources.walk7;
+                    }
+                    else if (pictureFrameCounter == 8)
+                    {
+                        this.picMan.Image = Properties.Resources.walk8;
+                    }
+                    else if (pictureFrameCounter == 9)
+                    {
+                        this.picMan.Image = Properties.Resources.walk9;
+                    }
+                    else if (pictureFrameCounter == 10)
+                    {
+                        this.picMan.Image = Properties.Resources.walk10;
+                    }
+
+                    //Add 1 to the counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+
+                    //Refresh the screen
+                    this.Refresh();
+
+                    //pause the loop for 50 milliseconds
+                    Thread.Sleep(50);
+                }
+            }
+        }
+    }
+}
