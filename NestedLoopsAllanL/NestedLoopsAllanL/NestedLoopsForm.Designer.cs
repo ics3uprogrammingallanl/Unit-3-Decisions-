7c877141fd977544c2d09ff9a2faf1e41b5bdb77
AL10120 <allan.leveille@ocsbstudent.ca>
2019-04-02 12:54:37 -0400

    NL complete want operational

---
+++ NestedLoopsAllanL/NestedLoopsAllanL/NestedLoopsForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace NestedLoopsAllanL
+{
+    partial class frmNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(85, 26);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(156, 60);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.Location = new System.Drawing.Point(62, 92);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(201, 186);
+            this.lstLetters.TabIndex = 1;
+            // 
+            // frmNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 314);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoops";
+            this.Text = "Nested Loops by Allan L";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstLetters;
+    }
+}
+
---
+++ NestedLoopsAllanL/NestedLoopsAllanL/NestedLoopsForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoopsAllanL
+{
+    public partial class frmNestedLoops : Form
+    {
+        public frmNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int CapLetterCounter;
+            int LowLetterCounter;
+            string CapLetters;
+            string LowLetters;
+
+            //clear ListBox
+            lstLetters.Items.Clear();
+
+            for (CapLetterCounter = 65; CapLetterCounter <=90; CapLetterCounter++)
+            {
+                for (LowLetterCounter = 97; LowLetterCounter <= 122; LowLetterCounter++)
+                {
+                    CapLetters = char.ConvertFromUtf32 (CapLetterCounter);
+                    LowLetters = char.ConvertFromUtf32(LowLetterCounter);
+
+                    lstLetters.Items.Add(CapLetters + " -> " + LowLetters);
+                }
+                    
+            }
+
+        }
+    }
+}
