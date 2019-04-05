12786ad2303e48c7d448c649b8d6058b108166dd
AL10120 <allan.leveille@ocsbstudent.ca>
2019-04-05 11:43:36 -0400

    CO complete

---
+++ ControlObjectsAllanL/ControlObjectsAllanL/ControlObjectsFrom.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ControlObjectsAllanL
+{
+    partial class frmControlObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColour = new System.Windows.Forms.Button();
+            this.lblColour = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnColour
+            // 
+            this.btnColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColour.Location = new System.Drawing.Point(54, 36);
+            this.btnColour.Name = "btnColour";
+            this.btnColour.Size = new System.Drawing.Size(124, 53);
+            this.btnColour.TabIndex = 0;
+            this.btnColour.Text = "Change Colour";
+            this.btnColour.UseVisualStyleBackColor = true;
+            this.btnColour.Click += new System.EventHandler(this.btnColour_Click);
+            // 
+            // lblColour
+            // 
+            this.lblColour.AutoSize = true;
+            this.lblColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColour.Location = new System.Drawing.Point(50, 132);
+            this.lblColour.Name = "lblColour";
+            this.lblColour.Size = new System.Drawing.Size(128, 20);
+            this.lblColour.TabIndex = 1;
+            this.lblColour.Text = "Change Colour";
+            // 
+            // frmControlObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 212);
+            this.Controls.Add(this.lblColour);
+            this.Controls.Add(this.btnColour);
+            this.Name = "frmControlObjects";
+            this.Text = "Control Objects by Allan L";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColour;
+        private System.Windows.Forms.Label lblColour;
+    }
+}
+
---
+++ ControlObjectsAllanL/ControlObjectsAllanL/ControlObjectsFrom.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by: Allan L
+ * Created on: April 4, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #25 - Control Objects
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlObjectsAllanL
+{
+    public partial class frmControlObjects : Form
+    {
+        public frmControlObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColour_Click(object sender, EventArgs e)
+        {
+            //change the backround color of label to deep sky blue 
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.DeepSkyBlue;
+
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.DeepSkyBlue;
+                }
+            }
+        }
+    }
+}
